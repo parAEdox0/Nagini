1f595835bc971799cefdadb7cd32f281c1e535ca
Parth Ekbote <paraedox0@gmail.com>
2023-08-28 20:05:40 +0530

    Assignment Q1
    To enter marks of the students

--- leap_year.py
+++ leap_year.py
@@ -1,5 +1,4 @@
 def is_leap(year):
-    
     if year % 4 == 0:
         if year % 100 == 0 and year % 400 == 0:
             return True
@@ -9,7 +8,5 @@ def is_leap(year):
             return True
     else:
         return False
-
-
 year = int(input())
 print(is_leap(year))
---
+++ marks.py
@@ -0,0 +1,9 @@
+marks = []
+present = []
+absent = []
+num = int(input("ENTER NUMBER OF STUDENTS : "))
+for i in range(1, num+1):
+    val = int(input(f"ENTER MARKS OBTAINED BY ROLL NUMBER {i} : "))
+    marks.append(val)
+print(marks)
+    
