cce9dd38d9131ab6069de97a1caad251ed0e7487
Parth Ekbote <paraedox0@gmail.com>
2023-08-28 23:17:41 +0530

    Completed

--- marks.py
+++ marks.py
@@ -2,11 +2,11 @@ marks = []
 present = []
 absent = []
 num = int(input("ENTER NUMBER OF STUDENTS : "))
+print()
 
 for i in range(1, num+1):
     val = input(f"ENTER MARKS OBTAINED BY ROLL NUMBER {i} : ")
     marks.append(val)
-    # print(marks)
 k = 1
 for j in marks:
     if j == "ab":       
@@ -14,6 +14,35 @@ for j in marks:
         k = k + 1
     else:
         present.append(j)
+print()
+print("THE NUMBER OF PRESENT STUDENTS = ", len(present))
+print("THE NUMBER OF ABSENT STUDENTS = ", len(absent))
+print()
 
-print("The Number of Present students = ", len(present))
-print("The Number of Absent students = ", len(absent))
+
+def avg():
+    sum_m = 0
+    for m in range(0, len(present)):
+        sum_m = int(sum_m) + int(present[m])
+    print("THE AVERAGE MARKS OF THE STUDENTS ARE = ", float(sum_m / len(present)))
+
+
+def high():
+    _high = 0
+    for n in range(0, len(present)):
+        if int(_high) < int(present[n]):
+            _high = present[n]
+    print("THE HIGHEST MARKS OF THE CLASS = ", _high)
+
+
+def low():
+    _low = 10
+    for n in range(0, len(present)):
+        if int(_low) > int(present[n]):
+            _low = present[n]
+    print("THE LOWEST MARKS OF THE CLASS = ", _low)
+
+
+avg()
+high()
+low()
