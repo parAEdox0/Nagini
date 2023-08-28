c13c56c1418ff7c0eebee5698a3adbb686a67410
Parth Ekbote <paraedox0@gmail.com>
2023-08-28 23:38:02 +0530

    Formatting

--- isprime.py
+++ isprime.py
@@ -1,10 +1,12 @@
-def isprime(num):
-    for i in range(2, num):
-        if num % i == 0:
+def isprime(_num):
+    for i in range(2, _num):
+        if _num % i == 0:
             print("The Number is not Prime")
             return
     else:
         print("The Number is Prime")
+
+
 print("Hello")
-num = int(input("Enter a Number : "))
-isprime(num)
+_num = int(input("Enter a Number : "))
+isprime(_num)
