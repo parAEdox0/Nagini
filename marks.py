71efbe7119b2f86601098a080ee39c3e699186a7
Parth Ekbote <paraedox0@gmail.com>
2023-08-28 22:20:38 +0530

    Pycharm

--- marks.py
+++ marks.py
@@ -2,8 +2,18 @@ marks = []
 present = []
 absent = []
 num = int(input("ENTER NUMBER OF STUDENTS : "))
+
 for i in range(1, num+1):
-    val = int(input(f"ENTER MARKS OBTAINED BY ROLL NUMBER {i} : "))
+    val = input(f"ENTER MARKS OBTAINED BY ROLL NUMBER {i} : ")
     marks.append(val)
-print(marks)
-    
+    # print(marks)
+k = 1
+for j in marks:
+    if j == "ab":       
+        absent.append(k)
+        k = k + 1
+    else:
+        present.append(j)
+
+print("The Number of Present students = ", len(present))
+print("The Number of Absent students = ", len(absent))
